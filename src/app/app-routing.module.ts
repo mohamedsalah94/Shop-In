ab91b7a1584f688dd4731d49af04ce44b2592631
mohamedsalah94 <70546224+mohamedsalah94@users.noreply.github.com>
2020-11-15 21:53:51 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { ProductsComponent } from './products/products.component';
 import { TermsComponent } from './terms/terms.component';
 
 const routes: Routes = [
- 
+ {path:'',redirectTo:'home',pathMatch:"full"},
+ {path:'terms',component:TermsComponent},
+  {path:'product',component:ProductsComponent},
 
 ];
 
