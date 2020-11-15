6609124a3ab62be273f70eddfa28007c2795579c
abdallahhesham99 <abdallahhesham414@gmail.com>
2020-11-15 17:39:00 +0200

    abdallah works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { HomeComponent } from './home/home.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckoutComponent,
+    HomeComponent,
+    UserDashboardComponent
   ],
   imports: [
     BrowserModule,
