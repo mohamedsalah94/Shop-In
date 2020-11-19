c95fa0421eb7d2eeb93f030c05fc2f82b17210a5
A7med Essam <madimodicom2@gmail.com>
2020-11-19 04:51:15 +0200

    remove counter from slider and fix search bar responsive

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -11,48 +11,48 @@ export class SliderComponent implements OnInit {
 
   ngOnInit(): void {
 
-    $('.carousel-control-next').click(changeNumbersOfSlider)
-    $('.carousel-control-prev').click(changeNumbersOfSlider2)
+    // $('.carousel-control-next').click(changeNumbersOfSlider)
+    // $('.carousel-control-prev').click(changeNumbersOfSlider2)
 
 
-    function changeNumbersOfSlider() {
+    // function changeNumbersOfSlider() {
 
-      if (    $('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/backpack_PNG6359.png" ) {
-        $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp  animate__faster'>2</div>`)
-      } 
-      else if($('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/backpack_PNG63591.png" ) {
-        $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>3</div>`)
-      }
-      else if($('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/jacket_PNG8052.png" ) {
-        $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>4</div>`)
-      }
-      else if($('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/polo_shirt_PNG8158.png" ) {
-        $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>5</div>`)
-      }
-      else if($('.carousel-item').filter(':visible').find('img').attr('src')== "../../assets/images/running_shoes_PNG5815.png" ) {
-        $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>1</div>`)
-      }
-    }
+    //   if (    $('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/backpack_PNG6359.png" ) {
+    //     $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp  animate__faster'>2</div>`)
+    //   } 
+    //   else if($('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/backpack_PNG63591.png" ) {
+    //     $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>3</div>`)
+    //   }
+    //   else if($('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/jacket_PNG8052.png" ) {
+    //     $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>4</div>`)
+    //   }
+    //   else if($('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/polo_shirt_PNG8158.png" ) {
+    //     $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>5</div>`)
+    //   }
+    //   else if($('.carousel-item').filter(':visible').find('img').attr('src')== "../../assets/images/running_shoes_PNG5815.png" ) {
+    //     $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>1</div>`)
+    //   }
+    // }
 
 
-    function changeNumbersOfSlider2() {
+    // function changeNumbersOfSlider2() {
 
-      if (    $('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/backpack_PNG6359.png" ) {
-        $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>5</div>`)
-      } 
-      else if($('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/backpack_PNG63591.png" ) {
-        $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>1</div>`)
-      }
-      else if($('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/jacket_PNG8052.png" ) {
-        $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>2</div>`)
-      }
-      else if($('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/polo_shirt_PNG8158.png" ) {
-        $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>3</div>`)
-      }
-      else if($('.carousel-item').filter(':visible').find('img').attr('src')== "../../assets/images/running_shoes_PNG5815.png" ) {
-        $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>4</div>`)
-      }
-    }
+    //   if (    $('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/backpack_PNG6359.png" ) {
+    //     $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>5</div>`)
+    //   } 
+    //   else if($('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/backpack_PNG63591.png" ) {
+    //     $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>1</div>`)
+    //   }
+    //   else if($('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/jacket_PNG8052.png" ) {
+    //     $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>2</div>`)
+    //   }
+    //   else if($('.carousel-item').filter(':visible').find('img').attr('src') == "../../assets/images/polo_shirt_PNG8158.png" ) {
+    //     $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>3</div>`)
+    //   }
+    //   else if($('.carousel-item').filter(':visible').find('img').attr('src')== "../../assets/images/running_shoes_PNG5815.png" ) {
+    //     $('.numbers').html(`<div class='numbers animate__animated animate__fadeInUp animate__faster'>4</div>`)
+    //   }
+    // }
     
 
   }
